81a4ee498d23f4e39e68cacf0011bc3210aba19a
Daniil Zhuravlev <nakrovati@outlook.com>
2023-10-17 02:07:25 +0400

    chore: bump deps

--- src/lib/trpc/router.ts
+++ src/lib/trpc/router.ts
@@ -2,13 +2,13 @@ import type { Context } from "$lib/trpc/context";
 
 import { db } from "$lib/db";
 import { initTRPC } from "@trpc/server";
-import { string } from "valibot";
+import { parse, string } from "valibot";
 
 export const t = initTRPC.context<Context>().create();
 
 export const router = t.router({
   findMonitors: t.procedure
-    .input(string())
+    .input((input) => parse(string(), input))
     .query(({ input }) => db.getDisplaysByString(input)),
 });
 
